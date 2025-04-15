a055c5a97af023b779570e3f4616f37c53e00e34
marcos p <marcos.p.mcruz@gmail.com>
2025-04-15 05:47:13 -0300

    product-03 - feat: add use case to search products by a category

--- service_product/internal/core/ports/product.usecase.go
+++ service_product/internal/core/ports/product.usecase.go
@@ -5,4 +5,5 @@ import "github.com/marcos-P-R/stock_manager/service_product/internal/core/model"
 type ProductUseCase interface {
 	Create(product *model.Product) (bool, error)
 	GetProductBySKU(sku string) (*model.Product, error)
+	GetProductsByCategory(category string) ([]*model.Product, error)
 }
